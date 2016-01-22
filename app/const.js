2d0d79d9df77f992567e53a0510fdcd71d2f0af4
Shujian <shujian.bu@gmail.com>
2016-01-21 21:32:12 -0600

    feat: add all media org and their logos

--- app/const.js
+++ app/const.js
@@ -9,19 +9,26 @@ const TOPICS = [
 ];
 
 const ORGS = [
-	{ value: '0',  label: 'all',                     ch: '' },
-	{ value: '1',  label: 'The New York Times',      ch: '纽约时报' },
-	{ value: '2',  label: 'The Guardian',            ch: '英国卫报' },
-	{ value: '3',  label: 'NPR',                     ch: '国家公共电台' },
-	{ value: '4',  label: 'ProPublica',              ch: '' },
-	{ value: '5',  label: 'The Wall Street Journal', ch: '华尔街日报' },
-	{ value: '6',  label: 'The Washington Post',     ch: '华盛顿邮报' },
-	{ value: '7',  label: 'Bloomberg',               ch: '彭博社' },
-	{ value: '8',  label: 'Five Thirty Eight',       ch: '538' },
-	{ value: '9',  label: 'Reuters',                 ch: '路透社' },
-	{ value: '11', label: 'Chicago Tribune',         ch: '芝加哥论坛报' },
-	{ value: '12', label: 'The Huffington Post',     ch: '赫芬顿邮报' },
-	{ value: '13', label: 'WNYC',                    ch: '纽约公共广播电台' }
+	{ value: '0',  label: 'all',             en:'',                            ch: '' },
+	{ value: '1',  label: 'nytimes',         en:'The New York Times',          ch: '纽约时报' },
+	{ value: '2',  label: 'guardian',        en:'The Guardian',                ch: '英国卫报' },
+	{ value: '3',  label: 'nprapps',         en:'NPR',                         ch: '国家公共电台' },
+	{ value: '4',  label: 'propublica',      en:'ProPublica',                  ch: '' },
+	{ value: '5',  label: 'WSJ',             en:'The Wall Street Journal',     ch: '华尔街日报' },
+	{ value: '6',  label: 'washingtonpost',  en:'The Washington Post',         ch: '华盛顿邮报' },
+	{ value: '7',  label: 'BloombergMedia',  en:'Bloomberg Media',             ch: '彭博社' },
+	{ value: '8',  label: 'FiveThirtyEight', en:'Five Thirty Eight',           ch: '538' },
+	{ value: '9',  label: 'thomsonreuters',  en:'Thomson Reuters',             ch: '路透社' },
+	{ value: '10', label: 'TimeMagazine',    en:'Time Magazine',               ch: '时代' },
+	{ value: '11', label: 'newsapps',        en:'Chicago Tribune',             ch: '芝加哥论坛报' },
+	{ value: '12', label: 'huffpostdata',    en:'The Huffington Post',         ch: '赫芬顿邮报' },
+	{ value: '13', label: 'wnyc',            en:'WNYC',                        ch: '纽约公共广播电台' },
+	{ value: '14', label: 'datadesk',        en:'Los Angeles Times Data Desk', ch: '洛杉矶时报' },
+	{ value: '15', label: 'BuzzFeedNews',    en:'Buzz Feed',                   ch: '' },
+	{ value: '16', label: 'ajam',            en:'Al Jazeera America',          ch: '半岛电台美国' },
+	{ value: '17', label: 'voxmedia',        en:'Vox Media',                   ch: '' },
+	{ value: '18', label: 'censusreporter',  en:'Census Reporter',             ch: '' },
+	{ value: '19', label: 'dukechronicle',   en:'The Duke Chronicle',          ch: '' }
 ];
 
 const TYPES = [
